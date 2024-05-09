b1fd66e68d07e35d8e449acf732c2754934275bf
Mohammed Uvaise <mohammeduvaisec@gmail.com>
2024-05-09 14:22:35 +0530

    fff

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/fav.png" />
       </head>
       <body>
         <Header />
